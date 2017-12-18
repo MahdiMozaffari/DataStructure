095bfc87dd38f85d58036b0208a21adb6f979049
Mahdi <mahdimozaffari1395@gmail.com>
2017-12-18 21:33:58 +0330

    upload Graph2 _SlidePart6_1

---
+++ Graph 2  _  Slide ( part 6_1)/src/Graph_2/Graph.java
@@ -0,0 +1,8 @@
+// the name : Mahdi_Mozaffari
+// the_collegian number : 9511213529
+
+package Graph_2;
+
+public class Graph {
+
+}
---
+++ Graph 2  _  Slide ( part 6_1)/src/Graph_2/Main.java
@@ -0,0 +1,12 @@
+// the name : Mahdi_Mozaffari
+// the_collegian number : 9511213529
+package Graph_2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
